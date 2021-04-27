8315ee69e60c70b1de97677c0b7ceeebb5a95c91
EricEOL <lopes.eric051@gmail.com>
2021-04-27 20:53:17 -0300

    Aplicando responsividade no Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,22 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({showProfileData = true}: ProfileProps) {
     return (
         <Flex align="center">
-        <Box mr="4" textAlign="right">
-            <Text>Eric Lopes</Text>
-            <Text 
-                color="gray.300"
-                fontSize="small"
-            >lopes.eric051@gmail.com</Text>
-        </Box>
+        
+        {showProfileData && (
+                    <Box mr="4" textAlign="right">
+                    <Text>Eric Lopes</Text>
+                    <Text 
+                        color="gray.300"
+                        fontSize="small"
+                    >lopes.eric051@gmail.com</Text>
+                </Box>
+        )}
 
         <Avatar size="md" name="Eric Lopes" src="https://github.com/EricEOL.png"/>
     </Flex>
