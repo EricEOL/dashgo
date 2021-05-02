149ee4af0fe2eb4189ad30c68a785643165c1f50
EricEOL <lopes.eric051@gmail.com>
2021-05-02 19:20:51 -0300

    Utilizando a lib react-hook-form na tea de sign in

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,25 +1,28 @@
-import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps} from '@chakra-ui/react';
+import { forwardRef, ForwardRefRenderFunction } from 'react';
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react';
 
-
-interface InputProps extends ChakraInputProps{
+interface InputProps extends ChakraInputProps {
     name: string;
     label?: string;
-} 
+}
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) => {
     return (
         <FormControl>
             {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-            <ChakraInput 
-                id={name} 
-                name={name} 
-                focusBorderColor="pink.500" 
-                bgColor="gray.900" 
-                variant="filled" 
-                _hover={{ bgColor: 'gray.900' }} 
+            <ChakraInput
+                id={name}
+                name={name}
+                focusBorderColor="pink.500"
+                bgColor="gray.900"
+                variant="filled"
+                _hover={{ bgColor: 'gray.900' }}
                 size="lg"
-                {...rest} 
+                ref={ref}
+                {...rest}
             />
         </FormControl>
     );
 }
+
+export const Input = forwardRef(InputBase);
