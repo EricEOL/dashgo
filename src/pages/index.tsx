6a15b2530a333c0c98fef802cf72416cfe65135a
EricEOL <lopes.eric051@gmail.com>
2021-05-03 21:41:25 -0300

    Validação de erros da tela de sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import { Flex, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Flex, Button, Stack } from '@chakra-ui/react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Input } from '../components/Form/Input';
 
 type SignInFormData = {
@@ -7,24 +9,32 @@ type SignInFormData = {
   password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória')
+})
+
 export default function SignIn() {
 
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+  const { errors } = formState;
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
-    
+
     console.log(values);
   }
 
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
 
-      <Flex as="form" 
-        w="100%" 
-        maxWidth={360} 
-        bg="gray.800" 
-        p="8" borderRadius={8} 
+      <Flex as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8" borderRadius={8}
         flexDir="column"
         onSubmit={handleSubmit(handleSignIn)}
       >
@@ -35,6 +45,7 @@ export default function SignIn() {
             type="email"
             name="email"
             label="E-mail"
+            error={errors.email}
             {...register('email')}
           />
 
@@ -42,15 +53,16 @@ export default function SignIn() {
             type="password"
             name="password"
             label="Senha"
+            error={errors.password}
             {...register('password')}
           />
 
         </Stack>
 
-        <Button 
-          type="submit" 
-          mt="6" 
-          colorScheme="pink" 
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
           size="lg"
           isLoading={formState.isSubmitting}
         >
