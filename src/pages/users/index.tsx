b3afd0eb04d91a6b5a4197ebb34ef9fe69d43b13
EricEOL <lopes.eric051@gmail.com>
2021-05-10 20:18:35 -0300

    Utilizando react-query na página de users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,23 +1,24 @@
 import Link from 'next/link';
-import { Box, Flex, Heading, Button, Text, Icon, Table, Thead, Tbody, Tr, Th, Td, Checkbox, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Flex, Heading, Button, Text, Icon, Table, Thead, Tbody, Tr, Th, Td, Checkbox, useBreakpointValue, Spinner } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
-import { useEffect } from 'react';
+import { useQuery } from 'react-query';
 
 export default function UserList() {
+
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users');
+        const data = await response.json();
+
+        return data;
+    })
 
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
     })
-
-    useEffect(()=>{
-        fetch('http://localhost:3000/api/users')
-        .then(response => response.json())
-        .then(data => console.log(data));
-    }, [])
 
     return (
         <Box>
@@ -35,24 +36,34 @@ export default function UserList() {
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
-                        
+
                         <Link href="/users/create" passHref>
-                        <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
-                            Criar novo
+                            <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
+                                Criar novo
                         </Button>
-                        </Link> 
+                        </Link>
 
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>
-                                    Usuário
-                                </Th>
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                        <Table colorScheme="whiteAlpha">
+                            <Thead>
+                                <Tr>
+                                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                                        <Checkbox colorScheme="pink" />
+                                    </Th>
+                                    <Th>
+                                        Usuário
+                                    </Th>
 
                                 {isWideVersion && <Th>Data de cadastro</Th>}
 
@@ -75,17 +86,20 @@ export default function UserList() {
                                 {isWideVersion &&
                                     <Td>
                                         <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                                            Editar
+                            Editar
                                     </Button>
                                     </Td>
                                 }
                             </Tr>
                         </Tbody>
-                    </Table>
+                    </Table >
 
                     <Pagination />
-                </Box>
-            </Flex>
-        </Box>
+                    </>
+                    )
+}
+                </Box >
+            </Flex >
+        </Box >
     )
 }
