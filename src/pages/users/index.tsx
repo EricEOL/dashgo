ec0fd498b1a6d6ac1cc25bcbe67ee83770fef2e9
EricEOL <lopes.eric051@gmail.com>
2021-05-13 20:34:27 -0300

    Spinner para refetching de dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -8,11 +8,26 @@ import { useQuery } from 'react-query';
 
 export default function UserList() {
 
-    const { data, isLoading, error } = useQuery('users', async () => {
+    const { data, isLoading, isFetching, error } = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users');
         const data = await response.json();
 
-        return data;
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        })
+
+        return users;
+    },{
+        staleTime: 1000 * 5, // 5 seconds
     })
 
     const isWideVersion = useBreakpointValue({
@@ -35,7 +50,10 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
 
                         <Link href="/users/create" passHref>
                             <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
@@ -55,49 +73,53 @@ export default function UserList() {
                         </Flex>
                     ) : (
                         <>
-                        <Table colorScheme="whiteAlpha">
-                            <Thead>
-                                <Tr>
-                                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                                        <Checkbox colorScheme="pink" />
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>
+                                            Usuário
                                     </Th>
-                                    <Th>
-                                        Usuário
-                                    </Th>
 
-                                {isWideVersion && <Th>Data de cadastro</Th>}
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
 
-                                <Th w="8"></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Eric Lopes</Text>
-                                        <Text fontSize="sm" color="gray.300">lopes.eric051@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de abril, 2021</Td>}
+                                        <Th w="8"></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
 
-                                {isWideVersion &&
-                                    <Td>
-                                        <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                            Editar
-                                    </Button>
-                                    </Td>
-                                }
-                            </Tr>
-                        </Tbody>
-                    </Table >
+                                                {isWideVersion &&
+                                                    <Td>
+                                                        <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                                            Editar
+                                        </Button>
+                                                    </Td>
+                                                }
+                                            </Tr>
+                                        )
+                                    })}
+                                </Tbody>
+                            </Table >
 
-                    <Pagination />
-                    </>
+                            <Pagination />
+                        </>
                     )
-}
+                    }
                 </Box >
             </Flex >
         </Box >
